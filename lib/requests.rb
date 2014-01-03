e96a2c0f1f59247805a159aeadf1ef21cb010431
John Wakeling <jwakeling23@gmail.com>
2014-01-03 09:51:57 +0000

    making JSON parser a private method
    making JSON parser a private method

--- lib/requests.rb
+++ lib/requests.rb
@@ -5,21 +5,27 @@ module RestClientPlus
     def self.post_json_to_url input_url, json_body
       response = RestClient.post(input_url, json_body, :content_type => 'application/json')
       response.code.should == 200
-      JSON.parse response.body
+      parse_response_body(response)
     end
 
     def self.put_json_to_url input_url, json_body
       response = RestClient.put(input_url, json_body, :content_type => 'application/json')
       response.code.should == 200
-      JSON.parse response.body
+      parse_response_body(response)
     end
 
     def self.get_json_from_url input_url
       response = RestClient.get(input_url)
       response.code.should == 200
+      parse_response_body(response)
+    end
+	
+	private
+
+    def self.parse_response_body(response)
       JSON.parse response.body
     end
 
   end
-
+  
 end
