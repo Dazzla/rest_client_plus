671ffdc2b3f99960df16c6e5a44e3b97591ae826
Dazzla <dazzlebown@gmail.com>
2014-01-07 17:45:50 +0000

    Removing debug

--- lib/requests.rb
+++ lib/requests.rb
@@ -3,19 +3,27 @@ module RestClientPlus
   module Requests
 
     def self.post_json_to_url input_url, json_body
-      parse_response(RestClient.post(input_url, json_body, :content_type => 'application/json'))
+      rsp = parse_response(RestClient.post(input_url, json_body, :content_type => 'application/json'))
+      $stdout.puts "RAW: #{rsp.class}"
+      rsp
     end
 
     def self.put_json_to_url input_url, json_body
-      parse_response(RestClient.put(input_url, json_body, :content_type => 'application/json'))
+      rsp = parse_response(RestClient.put(input_url, json_body, :content_type => 'application/json'))
+      $stdout.puts "RAW: #{rsp.class}"
+      rsp
     end
 
     def self.get_json_from_url input_url
-      parse_response(RestClient.get(input_url))
+      rsp = parse_response(RestClient.get(input_url))
+      $stdout.puts "RAW: #{rsp.class}"
+      rsp
     end
 
     def self.parse_response(response)
-      return response.respond_to?(:reverse) ? JSON.parse(response) : response
+      rsp = response.respond_to?(:reverse) ? JSON.parse(response) : response
+      $stdout.puts "Parsed: #{rsp.class}"
+      return rsp
     end
 
   end
